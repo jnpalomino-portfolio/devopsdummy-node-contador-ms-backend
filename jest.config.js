9ee8268881c82051aeca2217edf8e6729683b308
Stiven Manco Valencia <manco.5@hotmail.com>
2025-01-22 17:26:51 -0500

    Fix coverage issue

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    reporters: [
-      "default",
-      ["jest-junit", {
+  collectCoverage: true,
+  coverageReporters: ["lcov", "cobertura"],
+  testResultsProcessor: "jest-sonar-reporter",
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
         outputDirectory: "test-results",
         outputName: "test-results.xml",
-      }]
+      },
     ],
-    coverageReporters: ["lcov"]
-  };
+  ],
+};
