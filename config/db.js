fff18c29e111c73354e3b090e5f9e0070f4d0ca5
Karen Melissa Ramirez Casas <1000752146@medellin.gov.co>
2025-01-23 12:16:51 -0500

    se corrige conexion de bd

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({path: 'variables.env'})
 
 const conectarDB = async () => {
     try{
-        await mongoose.connect('mongodb://root:rootpassword@mongo:27017', {useNewUrlParser: true,
+        await mongoose.connect(process.env.DB_MONGO, {useNewUrlParser: true,
             useUnifiedTopology: true,
             authSource: 'admin'})
         console.log('DB conectada')
