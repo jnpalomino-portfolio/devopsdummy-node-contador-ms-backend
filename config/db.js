77218ed5fbf9c18caa1a97f57f616474e0b9163b
Karen Melissa Ramirez Casas <1000752146@medellin.gov.co>
2025-02-19 11:48:35 -0500

    se comenta motivo de crahs para verificar el error

--- config/db.js
+++ config/db.js
@@ -4,13 +4,15 @@ require('dotenv').config({path: 'variables.env'})
 const conectarDB = async () => {
     try{
         const connectionString = process.env.DB_MONGO;
+        console.log("DB_MONGO:", process.env.DB_MONGO);
         await mongoose.connect(connectionString, {useNewUrlParser: true,
             useUnifiedTopology: true,
             authSource: 'admin'})
         console.log('DB conectada')
-    } catch(error){
-        console.log('Error al conectar a MongoDB:', error);
-        process.exit(1);
-    } 
+    } catch (error) {
+        console.error('❌ Error al conectar a MongoDB:', error.message);
+        console.error('Stack trace:', error.stack);
+        // process.exit(1); <-- Comentado para evitar crash loop
+    }
 }
 module.exports = conectarDB  
